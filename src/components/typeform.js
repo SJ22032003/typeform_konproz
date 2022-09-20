6f4aaaf8a919e79ea1e04d5068ee831719fc1984
SJ22032003 <jaryalshaurya2203@gmail.com>
2022-09-20 17:03:45 +0530

    typeform testing

---
+++ src/components/typeform.js
@@ -0,0 +1,12 @@
+import { Widget } from "@typeform/embed-react";
+import { PopupButton } from "@typeform/embed-react";
+
+const form_id = "enECYEUz";
+
+export const MyComponent = () => {
+  return (
+    <PopupButton id={form_id} style={{ fontSize: 20 }} className="my-button">
+      Launch
+    </PopupButton>
+  );
+};
